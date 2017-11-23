83f788ec7b215d93dffe134c621f4d4d2ab7f2a0
Laura Gómez Rodríguez <Laura@MacBook-Pro-de-Laura.local>
2017-11-23 14:15:28 +0100

    Mi primer commit =D

---
+++ src/BowlingGame.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author Laura
+ *
+ */
+public class BowlingGame {
+	
+	//constructor
+	public BowlingGame (String game) {
+		
+	}
+	
+	public int getScore () {
+		
+		return (-1);
+		
+	}
+
+}
