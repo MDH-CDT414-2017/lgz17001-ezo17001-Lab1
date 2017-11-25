cdc59b9dcc42853382580c431d64ce09482090d6
Laura <ezornozamoreno@gmail.com>
2017-11-25 18:23:22 +0100

    Final Version: all especifications satisfied

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -27,16 +27,32 @@ public class BowlingGame {
 				return (-1);
 			}	
 		}
+		if(numbers.length <20 || numbers.length > 22) {
+			return -1;
+		}else if(numbers.length == 22) {
+			if(results[18] == 10){
+				int bonus = calculateBonus(results);
+				totalResult+=bonus;
+				return totalResult;
+			}else {
+				return -1;
+			}
+		}else if(numbers.length == 21 && results[18]+results[19] != 10) {
+				return -1;
+			
+		}else {
+			int bonus = calculateBonus(results);
+			totalResult+=bonus;
+			return totalResult;
+		}
 		
-		int bonus = calculateBonus(results);
-		totalResult+=bonus;
-		return totalResult;
 	}
+	//calculateBonus method
 	
 	public int calculateBonus (int[] arrayResults) {
 		int resultBonus = 0;
 		int bonusSum;
-		for(int i=0 ; i < arrayResults.length ; i++) {
+		for(int i=0 ; i < 18 ; i++) {
 			if(i%2==0) {
 				if(arrayResults[i] == 10) {
 					if(arrayResults[i+2] == 10) {
@@ -45,11 +61,12 @@ public class BowlingGame {
 					}else{	
 						bonusSum = (arrayResults[i+2] + arrayResults[i+3]);
 						resultBonus += bonusSum;
-					}
-				}else if ((arrayResults[i]+arrayResults[i+1]) == 10){
+						}
+					
+				}else if ((arrayResults[i]+arrayResults[i+1]) == 10){	
 					bonusSum = arrayResults[i+2];
-					resultBonus += bonusSum;
-				}	
+					resultBonus += bonusSum;	
+					}
 			}else {
 				resultBonus += 0;
 			}
--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -52,34 +52,69 @@ class BowlingGameTest {
 	}
 	
 	@Test
-	public void test2() {
-		BowlingGame bowlingGame = new BowlingGame("[1,5][2,3]");
-		assertEquals(11, bowlingGame.getScore());
+	public void testOpen() {
+		BowlingGame bowlingGame = new BowlingGame("[1,5][3,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6]");
+		assertEquals(81, bowlingGame.getScore());
 	}
 	@Test
-	public void test3() {
-		BowlingGame bowlingGame = new BowlingGame("[1,5][2,3][3,6]");
-		assertEquals(20, bowlingGame.getScore());
+	public void testStrike() {
+		BowlingGame bowlingGame = new BowlingGame("[10,0][3,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6]");
+		assertEquals(94, bowlingGame.getScore());
 	}
 	@Test
-	public void test4() {
-		BowlingGame bowlingGame = new BowlingGame("[1,5][10,0][3,6]");
-		assertEquals(34, bowlingGame.getScore());
+	public void testMultipleStrikes() {
+		BowlingGame bowlingGame = new BowlingGame("[10,0][10,0][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6]");
+		assertEquals(112, bowlingGame.getScore());
 	}
 	@Test
-	public void test5() {
-		BowlingGame bowlingGame = new BowlingGame("[10,0][10,0][7,2]");
-		assertEquals(55, bowlingGame.getScore());
+	public void testSpare() {
+		BowlingGame bowlingGame = new BowlingGame("[1,9][3,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6]");
+		assertEquals(88, bowlingGame.getScore());
 	}
 	@Test
-	public void test6() {
-		BowlingGame bowlingGame = new BowlingGame("[1,9][3,6]");
-		assertEquals(22, bowlingGame.getScore());
+	public void testMultipleSpares() {
+		BowlingGame bowlingGame = new BowlingGame("[8,2][5,5][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6]");
+		assertEquals(98, bowlingGame.getScore());
 	}
 	@Test
-	public void test7() {
-		BowlingGame bowlingGame = new BowlingGame("[1,9][4,6][3,2]");
-		assertEquals(32, bowlingGame.getScore());
+	public void testStrikeSpare() {
+		BowlingGame bowlingGame = new BowlingGame("[10,0][4,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6]");
+		assertEquals(103, bowlingGame.getScore());
+	}
+	@Test
+	public void testLastSpare() {
+		BowlingGame bowlingGame = new BowlingGame("[1,5][3,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,8][7]");
+		assertEquals(90, bowlingGame.getScore());
+	}
+	@Test
+	public void testLastStrike() {
+		BowlingGame bowlingGame = new BowlingGame("[1,5][3,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][10,0][7,2]");
+		assertEquals(92, bowlingGame.getScore());
+	}
+	@Test
+	public void testPerfectGame() {
+		BowlingGame bowlingGame = new BowlingGame("[10,0][10,0][10,0][10,0][10,0][10,0][10,0][10,0][10,0][10,0][10,10]");
+		assertEquals(300, bowlingGame.getScore());
+	}
+	@Test
+	public void testCheating() {
+		BowlingGame bowlingGame = new BowlingGame("[10,0][10,0][10,0][10,0][10,0][10,0][10,0][10,0][10,0][9,0][7]");
+		assertEquals(-1, bowlingGame.getScore());
+	}
+	@Test
+	public void testCheating2() {
+		BowlingGame bowlingGame = new BowlingGame("[10,0][10,0][10,0][10,0][10,0][10,0][10,0][10,0][10,0][9,0][7,2]");
+		assertEquals(-1, bowlingGame.getScore());
+	}
+	@Test
+	public void testUnfinishedGame() {
+		BowlingGame bowlingGame = new BowlingGame("[10,0][10,0][10,0][10,0][10,0][10,0][10,0]");
+		assertEquals(-1, bowlingGame.getScore());
+	}
+	@Test
+	public void testTooManyFrames() {
+		BowlingGame bowlingGame = new BowlingGame("[10,0][10,0][10,0][10,0][10,0][10,0][10,0][3,6][8,1][1,2][3,4][5,2]");
+		assertEquals(-1, bowlingGame.getScore());
 	}
 	
 
